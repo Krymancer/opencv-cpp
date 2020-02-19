5f08a17c3fc7cb67cd0ae0f11423c79648549745
Júnior Nascimento <junior.nascm@gmail.com>
2020-02-19 20:18:50 -0300

    uploading

---
+++ Version/version.cpp
@@ -0,0 +1,10 @@
+#include <opencv4/opencv2/core.hpp>
+
+using namespace cv;
+using namespace std;
+int main(int argc, char** argv) {
+    //print opencv version
+    printf("opencv version: %d.%d.%d\n", CV_VERSION_MAJOR, CV_VERSION_MINOR, CV_VERSION_REVISION);
+
+    return 0;
+}
